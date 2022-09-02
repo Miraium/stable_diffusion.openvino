2f8efba975dca126857f9580642c9316b6cc3be7
Kengo Makino <ognek.m@gmail.com>
2022-09-03 01:08:40 +0900

    複数回実行スクリプトの追加

---
+++ multiple_generation.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import json
+import time
+import subprocess
+
+OUTPUT_ROOT = "output"
+BASE_COMMAND = 'python stable_diffusion.py --output "{output_path}" --prompt "{prompt}"'
+
+
+def main(setting):
+    with open(setting) as f:
+        json_obj = json.load(f)
+
+    os.makedirs(OUTPUT_ROOT, exist_ok=True)
+
+    products = json_obj["products"]
+
+    for product in products:
+        title = product["title"]
+        prompt = product["prompt"]
+        num_output = product["num_output"]
+
+        curr_time = time.strftime("%Y%m%d-%H-%M-%S")
+        product_dir = os.path.join(OUTPUT_ROOT, f"{curr_time}_{title}")
+        os.makedirs(product_dir, exist_ok=True)
+
+        setting_backup = os.path.join(product_dir, setting)
+        with open(setting_backup, 'w') as f:
+            json.dump(product, f)
+
+        for i in range(num_output):
+            image_name = f"{i:03d}.png"
+            output_path = os.path.join(product_dir, image_name)
+            command = BASE_COMMAND.format(output_path=output_path, prompt=prompt)
+            print(command)
+            subprocess.run(command, shell=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--setting", default="setting.json")
+    args = parser.parse_args()
+    main(args.setting)
